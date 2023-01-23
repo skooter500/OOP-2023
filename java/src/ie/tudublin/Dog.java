6c6d280211db5377b5eeaa754e92cfc18e01e114
Bryan Duggan <skooter500@gmail.com>
2023-01-23 23:34:26 +0000

    recording of class etc

---
+++ java/src/ie/tudublin/Dog.java
@@ -0,0 +1,24 @@
+package ie.tudublin;
+
+public class Dog
+{
+	String name;
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public Dog()
+	{
+	}
+	
+	public Dog(String name)
+	{
+	}
+	
+	public void speak()
+	{
+		System.out.println("Woof");
+	}
+}
---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,14 @@
+package ie.tudublin;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		
+		Dog penny = new Dog();
+		penny.setName("Penny");
+		penny.speak();
+	}
+	
+}
