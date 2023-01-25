254d60418bdf5ac461e9db53001ed99aeee5b967
Bryan Duggan <skooter500@gmail.com>
2023-01-25 16:06:51 +0000

    hp

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,6 +2,13 @@ package ie.tudublin;
 
 public class Main
 {
+
+	public static void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new HelloProcessing());
+    }
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
