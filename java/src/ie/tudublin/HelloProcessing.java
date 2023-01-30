a4c0a91759b8390ff9f91516b641bfbce163f4c3
Stephanie <stefniogu@gmail.com>
2023-01-30 12:24:08 +0000

    colours

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -10,10 +10,24 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
+		//colorMode(HSB);
+		background(0);
 		
 		
 	}
 	
 	public void draw(){	
+		stroke(255);
+		fill(0, 255, 255);
+		line(10, 10, 100, 100); //x1, y1, x2, y2
+		circle(100, 250, 70); //cx, cy, d
+		fill(255, 255, 0);
+		rect(10, 300, 10, 100); //tlx, tly, w, h
+		fill(0, 0, 255);
+		noStroke();
+		noFill();
+		strokeWeight(15);
+		triangle(40, 90, 300, 20, 90, 70);
+		stroke(127);
 	}
 }
