2c387fd44d6f98724fe14e1019256913bbd31b1f
Stephanie <stefniogu@gmail.com>
2023-01-30 12:49:20 +0000

    Illuminate colours task

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -4,30 +4,44 @@ import processing.core.PApplet;
 
 public class HelloProcessing extends PApplet
 {
-
+	int screenSize = 500;
+	
 	public void settings(){
 		size(500, 500);
 	}
 
 	public void setup() {
-		//colorMode(HSB);
-		background(0);
+		background(255, 0, 0);
 		
 		
 	}
 	
 	public void draw(){	
-		stroke(255);
-		fill(0, 255, 255);
-		line(10, 10, 100, 100); //x1, y1, x2, y2
-		circle(100, 250, 70); //cx, cy, d
-		fill(255, 255, 0);
-		rect(10, 300, 10, 100); //tlx, tly, w, h
-		fill(0, 0, 255);
 		noStroke();
-		noFill();
-		strokeWeight(15);
-		triangle(40, 90, 300, 20, 90, 70);
-		stroke(127);
+		fill(255, 255, 0);
+		circle(screenSize/2, screenSize/2, screenSize/100*90);
+		fill(0, 255, 255);
+		triangle(0, 400, screenSize, 400, 255, 0);
+		fill(255, 255, 255);
+		ellipse(screenSize/2, screenSize/2, 300, 100);
+		fill(1, 1, 1);
+		circle(screenSize/2, screenSize/2, screenSize/100*25);
+		fill(mouseX, mouseY, mouseY);
+
+		circle(mouseX, mouseY, 50);
+
+
+		// stroke(255);
+		// fill(0, 255, 255);s
+		// line(10, 10, 100, 100); //x1, y1, x2, y2
+		// circle(100, 250, 70); //cx, cy, d
+		// fill(255, 255, 0);
+		// rect(10, 300, 10, 100); //tlx, tly, w, h
+		// fill(0, 0, 255);
+		// noStroke();
+		// noFill();
+		// strokeWeight(15);
+		// triangle(40, 90, 300, 20, 90, 70);
+		// stroke(127);
 	}
 }
