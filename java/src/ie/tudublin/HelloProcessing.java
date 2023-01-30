6599827c70821042aa48c26bb924d244ac664403
Bakht <C21416022@mytudublin.ie>
2023-01-30 12:49:31 +0000

    All seeing eye

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,20 +11,10 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB);
 		background(0);
 
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
+		
 
 		smooth();
 		
@@ -36,6 +26,40 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-	
+		//stroke(255);
+		//line(10,10,100,100); // x1,y1,x2,y2
+		//circle (300,250,70); // cx,cy,diam
+		//fill(0,255,0);
+		//rect(10,300,10,100); // top leftx , top left y , width , height 
+		//stroke(127);
+		//noStroke();
+		//noFill();
+		// fill(0,0,255);
+		//triangle (30,90,300,20,80,70);
+		//fill(0,0,255); 
+		
+		// making wierd thing
+		fill(255,0,0);
+		rect(0,0,900,900); // top leftx , top left y , width , height 
+		fill(255,255,0);
+		circle (250,300,450); // cx,cy,diam
+		fill(0,250,255);
+		triangle (30,450,470,450,250,0); // p1x,p1y,p2x,p2y,p3x,p3y
+		fill(255,255,255);
+		ellipse(250,230,180,110); // ex,ey,p1y,p2y
+		fill(0,0,0);
+		circle (250,230,80); // cx,cy,diam
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
