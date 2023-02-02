cdc106ed206dc90be26c462efc5665efd3b237a8
Bryan Duggan <skooter500@gmail.com>
2023-02-02 13:45:12 +0000

    bla

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,6 +5,13 @@ import processing.core.PApplet;
 public class BugZap extends PApplet
 {
 
+	float playerX, playerY, playerWidth;
+	float bugX, bugY, bugWidth;
+
+	float halfPlayer, halfBug;
+	
+	int score = 0;
+
 	public void settings()
 	{
 		size(500, 500);
@@ -13,55 +20,57 @@ public class BugZap extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
 		smooth();
 		
+
+		playerX = width * 0.5f;
+		playerY = height - 100;
+
+		playerWidth = 50;
+		halfPlayer = playerWidth * 0.5f;
+
+		resetBug();
+
 	}
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+	void resetBug()
+	{
+		bugY = 50;
+		bugWidth = 50;
+		halfBug = bugWidth * 0.5f;
+		bugX = random(halfBug, width - halfBug);
+	}
+
+	void drawPlayer(float x, float y, float w)
+	{
+		noFill();
+		stroke(255);
+		rectMode(CENTER);
+		rect(x, y, w, w);
+		line(x, y - halfPlayer, x, y - halfPlayer * 2);  
+	}
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			playerX --;
+		} 
+
+		if (keyCode == RIGHT)
+		{
+			playerX ++;
+		} 
+
+		if (key == ' ')
+		{
+			line(playerX, playerY - halfPlayer, playerX, 0);
+		}
+	}
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
-
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
-		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
-
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		background(0);
+		drawPlayer(playerX, playerY, playerWidth);
 	}
 }
