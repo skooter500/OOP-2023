24b1af2d9be5af9ca88621762f25abe4d9b9d414
Bryan Duggan <skooter500@gmail.com>
2023-02-15 14:56:30 +0000

    bla

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -12,14 +12,34 @@ public class StarMap extends PApplet
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-
+		
 		smooth();
 		
+
+
 	}
 
+
+	public void drawGrid()
+	{
+		stroke(255);
+		float border = 50.0f;
+
+		int count = 10;
+		float gap = (width - (border * 2.0f)) / (float) count;
+		for(int i = -5 ; i <= 5 ; i ++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+		}
+		
+	}
 		
 	public void draw()
 	{	
 		strokeWeight(2);		
+
+		drawGrid();
 	}
 }
