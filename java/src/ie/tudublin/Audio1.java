97b7dda883c037863df29554dab79e49c1581375
Bryan Duggan <skooter500@gmail.com>
2023-02-20 12:40:56 +0000

    bla

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -1,6 +1,5 @@
 package ie.tudublin;
 
-;
 import ddf.minim.AudioBuffer;
 import ddf.minim.AudioInput;
 import ddf.minim.AudioPlayer;
@@ -17,15 +16,20 @@ public class Audio1 extends PApplet
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 500);
 	}
+
+    int frameSize = 1024;
 
 	public void setup() {
 		colorMode(HSB);
 		background(0);
 
-		
+        minim = new Minim(this);
 
+        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
+        ab = ai.mix;
+		
 		smooth();
 		
 	}
@@ -34,6 +38,17 @@ public class Audio1 extends PApplet
 	
 	public void draw()
 	{	
-		
+		background(0);
+        stroke(255);
+
+        float half = height / 2;
+        float cgap = 255 / (float)ab.size();
+        for(int i = 0 ; i < ab.size() ; i ++)
+        {
+            stroke(cgap * i, 255, 255);
+            line(i, half, i , half + ab.get(i) * half); 
+        }
+
+        
 	}
 }
