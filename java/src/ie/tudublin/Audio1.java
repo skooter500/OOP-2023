9a90b3867a7ee1d98acee42c6b5e3a2ac6137047
matemikus <c21379483@mytudublin.ie>
2023-02-20 14:26:25 +0000

    changes

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -43,12 +43,26 @@ public class Audio1 extends PApplet
 
         float half = height / 2;
         float cgap = 255 / (float)ab.size();
+
+        float total = 0;
         for(int i = 0 ; i < ab.size() ; i ++)
         {
+            total += abs(ab.get(i)); // get every elemet of the array
             stroke(cgap * i, 255, 255);
             line(i, half, i , half + ab.get(i) * half); 
         }
+        float average = total / (float) ab.size();
+
+        float r = average * 200;
+
+        lerpedR = lerp(lerpedR, r, 0.1f);
+
+        circle(100,200,lerpedR);
+
+        // Lurp algorithm to spoothen it
 
         
 	}
+
+    float lerpedR = 0;
 }
