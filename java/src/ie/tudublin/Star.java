b1391e64a6a2cc4ae6f7537123b3ef147fa7ac2e
Prabhjeet Bissora <C21412282@mytudublin.ie>
2023-02-20 12:39:35 +0000

    star1

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -3,24 +3,24 @@ package ie.tudublin;
 import processing.data.Table;
 import processing.data.TableRow;
 
-public Star(TableRow tr)
-{
-    this(
-        tr.getInt("Hab?") == 1, 
-        tr.getString("Display Name"), 
-        tr.getFloat("Distance"),
-        tr.getFloat("Xg"),
-        tr.getFloat("Yg"),
-        tr.getFloat("Zg"),
-        tr.getFloat("AbsMag")
-    );
-    public Star (boolean hab; String displayName; float distance; float xG; float yG; float zG; float absMag;); {
-        this.hab = hab;
-        this.displayName = displayName;
-        this.distance = distance;
-        this.xG = xG;
-        this.yG = yG;
-        this.zG = zG;
-        this.absMag = absMag;
-    }
-}
+// public Star(TableRow tr)
+// {
+//     this(
+//         tr.getInt("Hab?") == 1, 
+//         tr.getString("Display Name"), 
+//         tr.getFloat("Distance"),
+//         tr.getFloat("Xg"),
+//         tr.getFloat("Yg"),
+//         tr.getFloat("Zg"),
+//         tr.getFloat("AbsMag")
+//     );
+//     public Star (boolean hab; String displayName; float distance; float xG; float yG; float zG; float absMag;); {
+//         this.hab = hab;
+//         this.displayName = displayName;
+//         this.distance = distance;
+//         this.xG = xG;
+//         this.yG = yG;
+//         this.zG = zG;
+//         this.absMag = absMag;
+//     }
+// }
