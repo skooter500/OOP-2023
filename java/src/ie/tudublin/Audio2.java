14677cb14682f820542b074ea4f28ebcff779492
matemikus <c21379483@mytudublin.ie>
2023-03-01 22:01:16 +0000

    changes

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -35,7 +35,7 @@ public class Audio2 extends PApplet{
     {
         float[] frequencies = {293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f, 587.33f, 659.25f, 739.99f, 783.99f, 880.00f, 987.77f, 1108.73f, 1174.66f};
 	    String[] spellings = {"D,", "E,", "F,", "G,", "A,", "B,", "C", "D", "E", "F", "G", "A", "B","c", "d", "e", "f", "g", "a", "b", "c'", "d'", "e'", "f'", "g'", "a'", "b'", "c''", "d''"};
-    }
+    } // PitchSpeller
 
     float[] lerpedBuffer;
     public void draw()
