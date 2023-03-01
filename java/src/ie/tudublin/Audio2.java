7e78c2e4a0ec3ef59f44ecaed52267befeacf40d
matemikus <c21379483@mytudublin.ie>
2023-03-01 22:01:35 +0000

    changes

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -31,7 +31,7 @@ public class Audio2 extends PApplet{
 
         fft = new FFT(width, 44100);
     }
-    public String PitchSpeller(float frequency)
+    public String PitchSpeller(float frequency) //Yup
     {
         float[] frequencies = {293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f, 587.33f, 659.25f, 739.99f, 783.99f, 880.00f, 987.77f, 1108.73f, 1174.66f};
 	    String[] spellings = {"D,", "E,", "F,", "G,", "A,", "B,", "C", "D", "E", "F", "G", "A", "B","c", "d", "e", "f", "g", "a", "b", "c'", "d'", "e'", "f'", "g'", "a'", "b'", "c''", "d''"};
