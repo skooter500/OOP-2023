b67d58be54671caaebb8751e1ecccff4bb3b2a3f
Bryan Duggan <skooter500@gmail.com>
2023-03-06 16:41:52 +0000

    life with bug

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -5,23 +5,24 @@ import processing.core.PApplet;
 public class Life extends PApplet
 {
 
+	LifeBoard board;
 	public void settings()
 	{
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
-
-		
-		
+		board = new LifeBoard(50, this);
+		board.randomise();
 	}
 
-	
-	
 	public void draw()
 	{	
+		background(0);
+		board.render();
+		board.applyRules();
 		
 	}
 }
