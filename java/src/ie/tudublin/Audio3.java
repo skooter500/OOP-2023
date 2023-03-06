59bf7f29df803c580641ee2823bd63db7c21449f
matemikus <c21379483@mytudublin.ie>
2023-03-06 11:47:55 +0000

    fsf

--- java/src/ie/tudublin/Audio3.java
+++ java/src/ie/tudublin/Audio3.java
@@ -17,6 +17,20 @@ public class Audio3 extends PApplet{
 
     FFT fft;
 
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            if (ap.isPlaying())
+            {
+                ap.pause();
+            }
+            else {
+                ap.play();
+            }
+        }
+    }
     public void settings()
     {
         size(1024, 1024, P3D);
@@ -28,19 +42,42 @@ public class Audio3 extends PApplet{
         ap = m.loadFile("tomp3.cc - 08 PsychNerD and Marco G  More Cowbell.mp3", 1024);
         // i = m.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ap.mix;
+        ap.play
         lerpedBuffer = new float[width];
 
         fft = new FFT(width, 44100);
     }
 
+    float rot = 0;
+    float lerpedAverage = 0;
+
     float[] lerpedBuffer;
     public void draw()
     {
+        float total = 0;
+        for (int i = 0; i < ab.size(); i++)
+        {
+            total += abs(ab.get(i));
+        }
+        float average = total / (float) ab.size();
+
+        lerpedAverage = lerpedAverage
         background(0);
+        lights();
         colorMode(HSB);
         stroke(255);
-        
-        box(100);
+        strokeWeight(5);
+
+        noFill(); // makes it a wireframe
+        translate(width / 2, height /2);
+        rotateY(rot);
+        rotateX(rot);
+
+        float boxSize = max(100, map(lerpedAverage, 0 , 1, 100, 500))
+
+        box(200);
+        rot += 0.01f;
+
     }
 
     float lerpedY = 0;
