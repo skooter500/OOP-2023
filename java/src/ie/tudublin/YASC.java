2efc939364df60b493ea04ccda4b4d579210133f
Bryan Duggan <skooter500@gmail.com>
2023-03-20 12:38:28 +0000

    bla

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -4,8 +4,9 @@ import processing.core.PApplet;
 
 public class YASC extends PApplet
 {
-
 	Ship ship;
+	Ship ship1;
+
 
 	public void settings()
 	{
@@ -15,13 +16,16 @@ public class YASC extends PApplet
 
 	public void setup() {
 		ship = new Ship(width / 2, height / 2, 50, 70, this);
+		ship1 = new Ship(100, 50, 80, 6, this);
 		colorMode(HSB);
 	}
 
-	
-	
 	public void draw()
-	{	
+	{	background(0);
 		ship.render();
+		ship.move();
+
+		ship1.render();
+		ship1.move();
 	}
 }
