d585957622c02c339631b502b284192cc2a98b66
Bryan Duggan <skooter500@gmail.com>
2023-03-20 11:41:24 +0000

    bla

---
+++ java/src/ie/tudublin/Ship.java
@@ -0,0 +1,50 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class Ship {
+    private PVector pos;
+    PApplet p;
+
+    public Ship(float x, float y, float size, int c, PApplet p)
+    {
+        pos = new PVector(x, y);
+        this.size = size;
+        this.c = c;       
+        this.p = p;  
+    }
+
+
+
+    public PVector getPos() {
+        return pos;
+    }
+    public void setPos(PVector pos) {
+        this.pos = pos;
+    }
+    public float getRot() {
+        return rot;
+    }
+    public void setRot(float rot) {
+        this.rot = rot;
+    }
+    public int getC() {
+        return c;
+    }
+    public void setC(int c) {
+        this.c = c;
+    }
+    public float getSize() {
+        return size;
+    }
+    public void setSize(float size) {
+        this.size = size;
+    }
+    private float rot;
+    private int c;
+    private float size;
+
+    
+    
+}
